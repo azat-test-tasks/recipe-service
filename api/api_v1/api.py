98666f8a19f1de135195d2a67b42b941d741f04b
temirovazat <azatbekt5@gmail.com>
2023-04-23 20:52:06 +0600

    update(api_v1): Update api.py

--- api/api_v1/api.py
+++ api/api_v1/api.py
@@ -1,7 +1,14 @@
 from fastapi import APIRouter
 
+from api.api_v1.endpoints.auth import login, registration
 from api.api_v1.endpoints.recipes import recipe
+from api.api_v1.endpoints.users import user
 
 api_router = APIRouter()
 
 api_router.include_router(recipe.router, prefix="/recipe", tags=["Recipes"])
+
+api_router.include_router(user.router, prefix="/users", tags=["User"])
+
+api_router.include_router(login.router, prefix="/auth", tags=["Authentication"])
+api_router.include_router(registration.router, prefix="/auth", tags=["Authentication"])
